f28569e1a9bc6609f7ea489f56ecfc0e7a12d2a3
Prathamesh Deshpande <prathameshdeshpande7@gmail.com>
2019-03-26 17:36:20 +0530

    v1.3 Handling 64 bit representation

--- print_binary.c
+++ print_binary.c
@@ -10,14 +10,14 @@
 #define PRINT_BINARY_PATTERN_INT8	"%c%c%c%c%c%c%c%c"
 
 #define PRINT_BYTE_TO_BINARY_INT8(i)	\
-	(((i) & 0x80) ? '1': '0'),		\
-	(((i) & 0x40) ? '1': '0'),		\
-	(((i) & 0x20) ? '1': '0'),		\
-	(((i) & 0x10) ? '1': '0'),		\
-	(((i) & 0x08) ? '1': '0'),		\
-	(((i) & 0x04) ? '1': '0'),		\
-	(((i) & 0x02) ? '1': '0'),		\
-	(((i) & 0x01) ? '1': '0')
+	(((i) & 0x80ll) ? '1': '0'),		\
+	(((i) & 0x40ll) ? '1': '0'),		\
+	(((i) & 0x20ll) ? '1': '0'),		\
+	(((i) & 0x10ll) ? '1': '0'),		\
+	(((i) & 0x08ll) ? '1': '0'),		\
+	(((i) & 0x04ll) ? '1': '0'),		\
+	(((i) & 0x02ll) ? '1': '0'),		\
+	(((i) & 0x01ll) ? '1': '0')
 
 #define PRINT_BINARY_PATTERN_INT16	\
 	PRINT_BINARY_PATTERN_INT8	PRINT_BINARY_SEPARATOR	PRINT_BINARY_PATTERN_INT8
@@ -30,10 +30,17 @@
 
 #define PRINT_BYTE_TO_BINARY_INT32(i)	\
 	PRINT_BYTE_TO_BINARY_INT16((i) >> 16),	PRINT_BYTE_TO_BINARY_INT16(i)
+
+#define PRINT_BINARY_PATTERN_INT64	\
+	PRINT_BINARY_PATTERN_INT32	PRINT_BINARY_SEPARATOR	PRINT_BINARY_PATTERN_INT32
+
+#define PRINT_BYTE_TO_BINARY_INT64(i)	\
+	PRINT_BYTE_TO_BINARY_INT32((i) >> 32), PRINT_BYTE_TO_BINARY_INT32(i)
+
 int main()
 {
-	long int a = 2147483647;
+	long long int a = 9223372036854775807;
 
-	printf("Binary representation of a: %lu is: "
-		PRINT_BINARY_PATTERN_INT32"\n", a, PRINT_BYTE_TO_BINARY_INT32(a));
+	printf("Binary representation of a: %llu is: "
+		PRINT_BINARY_PATTERN_INT64"\n", a, PRINT_BYTE_TO_BINARY_INT64(a));
 }
