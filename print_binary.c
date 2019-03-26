6d7ce30a16b11ef0d8d727603c1f070d83d20850
Prathamesh Deshpande <prathameshdeshpande7@gmail.com>
2019-03-26 17:12:34 +0530

    v1.1 Adding separator between bytes and INT16 macro

--- print_binary.c
+++ print_binary.c
@@ -5,21 +5,30 @@
  * a = 9 => 1001
  */
 #include <stdio.h>
-#define PRINT_BYTE_TO_BINARY_INT8(i)	\
-(((i) & 0x80) ? '1': '0'),		\
-(((i) & 0x40) ? '1': '0'),		\
-(((i) & 0x20) ? '1': '0'),		\
-(((i) & 0x10) ? '1': '0'),		\
-(((i) & 0x08) ? '1': '0'),		\
-(((i) & 0x04) ? '1': '0'),		\
-(((i) & 0x02) ? '1': '0'),		\
-(((i) & 0x01) ? '1': '0')
 
+#define PRINT_BINARY_SEPARATOR		" "
 #define PRINT_BINARY_PATTERN_INT8	"%c%c%c%c%c%c%c%c"
+
+#define PRINT_BYTE_TO_BINARY_INT8(i)	\
+	(((i) & 0x80) ? '1': '0'),		\
+	(((i) & 0x40) ? '1': '0'),		\
+	(((i) & 0x20) ? '1': '0'),		\
+	(((i) & 0x10) ? '1': '0'),		\
+	(((i) & 0x08) ? '1': '0'),		\
+	(((i) & 0x04) ? '1': '0'),		\
+	(((i) & 0x02) ? '1': '0'),		\
+	(((i) & 0x01) ? '1': '0')
+
+#define PRINT_BINARY_PATTERN_INT16	\
+	PRINT_BINARY_PATTERN_INT8	PRINT_BINARY_SEPARATOR	PRINT_BINARY_PATTERN_INT8
+
+#define PRINT_BYTE_TO_BINARY_INT16(i)	\
+	PRINT_BYTE_TO_BINARY_INT8((i) >> 8), PRINT_BYTE_TO_BINARY_INT8(i)
+
 int main()
 {
-	int a = 15;
+	int a = 0;
 
 	printf("Binary representation of a: %d is: "
-		PRINT_BINARY_PATTERN_INT8"\n", a, PRINT_BYTE_TO_BINARY_INT8(a));
+		PRINT_BINARY_PATTERN_INT16"\n", a, PRINT_BYTE_TO_BINARY_INT16(a));
 }
